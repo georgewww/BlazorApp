ddf8a68662622f6f391c854ef79b824d640b08d6
George Peppas <peppasgeorge@hotmail.com>
2020-05-05 15:26:06 +0300

    Adding mongodb

--- BlazorApp/Models/Customer.cs
+++ BlazorApp/Models/Customer.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace BlazorApp.Models
 {
     public class Customer
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
         public string CompanyName { get; set; }
         public string ContactName { get; set; }
